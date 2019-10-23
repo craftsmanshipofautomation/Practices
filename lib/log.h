e294712ac7877b049651d12d9580feb3d9fe205b
xierqi <xierqi>
2019-10-23 18:24:20 +0800

    log

--- lib/log.h
+++ lib/log.h
@@ -1,9 +1,9 @@
 #ifndef FTF_H
 #define FTF_H
 #include <limits.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdarg.h>
 
 #define escape \033
 
@@ -21,61 +21,56 @@
 #define Bold_Cyan "\033[1;36m"
 #define Reset "\033[0m"
 
-//static void _lzlog(const char *str, const char *type, char *fmt)
+// static void _lzlog(const char *str, const char *type, char *fmt)
 //{
 //   char type_char = type[0];
 //   snprintf(fmt, 256, "%s: %%%c", str, type_char);
 //}
 
-#define lzlog(EXPR, TYPE)                                        \
-   do                                                            \
-   {                                                             \
-      char _fmt[256];                                            \
-      char type_char = #TYPE[0];                                 \
-      snprintf(_fmt, 256, "%s: %%%c", #EXPR, type_char);         \
-      /*fprintf(stdout, "%s", _fmt);*/                           \
-      /*fprintf(stdout, "\n");*/                                 \
-      fprintf(stdout, Yellow);                                   \
-      fprintf(stdout, "%s:%d ", _##_FILE__, _##_LINE__);         \
-      fprintf(stdout, Reset);                                    \
-      fprintf(stdout, Cyan);                                     \
-      fprintf(stdout, " (%s:%d): ", _##_FUNCTION__, _##_LINE__); \
-      fprintf(stdout, Reset);                                    \
-      fprintf(stdout, _fmt, EXPR);                               \
-      fprintf(stdout, "\n");                                     \
-   } while (0)
+#define lzlog(EXPR, TYPE)                                                      \
+    do                                                                         \
+    {                                                                          \
+        fprintf(stdout, Yellow);                                               \
+        fprintf(stdout, "%s:%d ", _##_FILE__, _##_LINE__);                     \
+        fprintf(stdout, Reset);                                                \
+        fprintf(stdout, Cyan);                                                 \
+        fprintf(stdout, " (%s:%d): ", _##_FUNCTION__, _##_LINE__);             \
+        fprintf(stdout, Reset);                                                \
+        fprintf(stdout, "%s: %" #TYPE, #EXPR, EXPR);                           \
+        fprintf(stdout, "\n");                                                 \
+    } while (0)
 
-#define PRINT(name, arr, size)    \
-   {                              \
-      printf("%s: [ ", #name);    \
-      int i;                      \
-      for (i = 0; i < size; ++i)  \
-      {                           \
-         printf("%d ", (arr)[i]); \
-      }                           \
-      printf("]\n");              \
-   }
+#define PRINT(name, arr, size)                                                 \
+    {                                                                          \
+        printf("%s: [ ", #name);                                               \
+        int i;                                                                 \
+        for (i = 0; i < size; ++i)                                             \
+        {                                                                      \
+            printf("%d ", (arr)[i]);                                           \
+        }                                                                      \
+        printf("]\n");                                                         \
+    }
 
-#define PRINTF(name, arr, size)    \
-   {                              \
-      printf("%s: [ ", #name);    \
-      int i;                      \
-      for (i = 0; i < size; ++i)  \
-      {                           \
-         printf("%f ", (arr)[i]); \
-      }                           \
-      printf("]\n");              \
-   }
+#define PRINTF(name, arr, size)                                                \
+    {                                                                          \
+        printf("%s: [ ", #name);                                               \
+        int i;                                                                 \
+        for (i = 0; i < size; ++i)                                             \
+        {                                                                      \
+            printf("%f ", (arr)[i]);                                           \
+        }                                                                      \
+        printf("]\n");                                                         \
+    }
 
-#define PRINTC(name, arr, size)   \
-   {                              \
-      printf("%s: [ ", #name);    \
-      int i;                      \
-      for (i = 0; i < size; ++i)  \
-      {                           \
-         printf("%d ", (arr)[i]); \
-      }                           \
-      printf("]\n");              \
-   }
+#define PRINTC(name, arr, size)                                                \
+    {                                                                          \
+        printf("%s: [ ", #name);                                               \
+        int i;                                                                 \
+        for (i = 0; i < size; ++i)                                             \
+        {                                                                      \
+            printf("%d ", (arr)[i]);                                           \
+        }                                                                      \
+        printf("]\n");                                                         \
+    }
 
 #endif
