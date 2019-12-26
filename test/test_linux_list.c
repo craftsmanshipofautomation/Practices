62e5e92138c8bfefe3b2fdfe4333eb1f17f453a8
kljfzos <lkjfs2i_e>
2019-12-26 19:05:28 +0800

    hard as hell that double linked list

--- test/test_linux_list.c
+++ test/test_linux_list.c
@@ -1,12 +1,31 @@
 #include "list.h"
 #include <assert.h>
 
-// Implement appending functionality
+// https://kernelnewbies.org/FAQ/LinkedLists
 
-
-
+struct mystruct {
+    int data;
+    struct list_head mylist;
+};
 
 int main() 
 {
+    struct mystruct a = {
+        .data = 1,
+        .mylist = LIST_HEAD_INIT(a.mylist)
+    };
+
+    struct mystruct b = {
+        .data = 2,
+        .mylist = LIST_HEAD_INIT(b.mylist)
+    };
+    
+    LIST_HEAD(mylinkedlist);
+    list_add (&a.mylist, &mylinkedlist);
+    list_add (&b.mylist, &mylinkedlist);
+    
+
+
+
 
 }
