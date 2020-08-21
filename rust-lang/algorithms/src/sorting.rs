2a0e9564760725469022ee06f558f886699324a0
ruscal <ruscal@__crater__>
2020-08-21 18:24:01 +0800

    boring

--- rust-lang/algorithms/src/sorting.rs
+++ rust-lang/algorithms/src/sorting.rs
@@ -1,10 +1,16 @@
 pub mod sorting {
-    enum SCNET{
+    pub enum SCENT{
         ASCENT,
         DESCENT,
     }
+    pub fn swap<T: Clone>(i: &mut T, j: &mut T) {
+        let temp: T = i.clone();
+        *i = j.clone();
+        *j = temp;
+
+    }
     // direction: 0: ascent 1: 
-    pub fn bubble_sort(arr: &mut [i32], scent: enum SCENT) {
+    pub fn bubble_sort(arr: &mut [i32], scent: SCENT) {
 
     }
 }
